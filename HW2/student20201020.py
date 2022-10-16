6a6477038fe478772f3cb8052cba28f37f4fa629
jiminseon <pitapatsun@gmail.com>
2022-10-17 02:13:53 +0900

    add HW2_3

--- HW2/student20201020.py
+++ HW2/student20201020.py
@@ -25,35 +25,42 @@ for row in ws:
 		sum_v += ws.cell(row = row_id, column = 6).value
 		ws.cell(row = row_id, column = 7).value = sum_v
 		total = ws.cell(row = row_id, column = 7).value
+		print(total)
 		if total > A1[0]:
 			A1[0] = total
 			A1.sort()
 			ws.cell(row = row_id, column = 8).value = 'A+'
+			row_id += 1
 			continue
 		elif total > A2[0]:
 			A2[0] = total
 			A2.sort()
 			ws.cell(row = row_id, column = 8).value = 'A0'
+			row_id += 1
 			continue
 		elif total > B1[0]:
 			B1[0] = total
 			B1.sort()
 			ws.cell(row = row_id, column = 8).value = 'B+'
+			row_id += 1
 			continue		
 		elif total > B2[0]:
 			B2[0] = total
 			B2.sort()
 			ws.cell(row = row_id, column = 8).value = 'B0'
+			row_id += 1
 			continue	
 		elif total > C1[0]:
 			C1[0] = total
 			C1.sort()
 			ws.cell(row = row_id, column = 8).value = 'C+'
+			row_id += 1
 			continue	
 		elif total > C2[0]:
 			C2[0] = total
 			C2.sort()
 			ws.cell(row = row_id, column = 8).value = 'C0'
+			row_id += 1
 			continue
 	row_id += 1
 

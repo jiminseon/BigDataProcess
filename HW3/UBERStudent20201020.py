bc1113a9c0c1165659c99e400d85f93c58c6a291
jiminseon <pitapatsun@gmail.com>
2022-11-02 01:12:47 +0900

    UBER2

--- HW3/UBERStudent20201020.py
+++ HW3/UBERStudent20201020.py
@@ -7,17 +7,33 @@ output = sys.argv[2]
 
 with open(output, "wt") as wp:
 	with open(input, "rt") as rp:
-		twoList = []
+		a = dict()
 		dayofweek = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
 		for line in rp:
 			str_arr = line.strip()
 			str_arr = str_arr.split(",")
 			date = str_arr[1].split("/")
 			str_arr[1] = dayofweek[calendar.weekday(int(date[2]), int(date[0]), int(date[1]))]
-			line = []
-			for i in range(len(str_arr)):
-				line.append(str_arr[i])
-			twoList.append(line)
+		
 
-		for i in range(len(twoList)):
-			wp.write(twoList[i][0] + "," + twoList[i][1] + " " + twoList[i][2] + "," + twoList[i][3] + "\n")
+			if (str_arr[0] not in a):
+				b = {str_arr[1] : str_arr[2], str_arr[1] + "t" : str_arr[3]}
+				a[str_arr[0]] = a.get(str_arr[0], b)
+			else:
+				if (str_arr[1] not in a[str_arr[0]]):
+					a[str_arr[0]][str_arr[1]] = str_arr[2] 
+					a[str_arr[0]][str_arr[1] + "t"] = str_arr[3]
+				else:
+					total = int(a[str_arr[0]][str_arr[1]]) + int(str_arr[2])
+					a[str_arr[0]][str_arr[1]] = str(total)
+					total = int(a[str_arr[0]][str_arr[1] + "t"]) + int(str_arr[3])
+					a[str_arr[0]][str_arr[1] + "t"] = str(total)
+
+		keylist = a.keys()	
+		for i in keylist:
+			for j in dayofweek:
+				vehicles = a[i][j]
+				trips = a[i][j + "t"]
+				wp.write(i + "," + j + " " +  vehicles + "," + trips + "\n")
+
+
